2f46fd68dd787b22c3352417b5de9a14f9299c5b
Nazmuz Pranto <npranto@gmail.com>
2022-06-10 18:50:21 -0400

    adds feature for issue #4 and #5

--- src/init.js
+++ src/init.js
@@ -1,59 +1,78 @@
-const { program } = require('commander');
-const addActionItem = require('./utils/addActionItem');
-const listActionItems = require('./utils/listActionItems');
-const markDoneActionItems = require('./utils/markDoneActionItems');
-const markUndoneActionItems = require('./utils/markUndoneActionItems');
-const removeActionItems = require('./utils/removeActionItems');
-
-// - program.command: takes a string that defines the format of the command
-// - program.description: describes the command for the user. This is helpful when the user executes our tool with the option --help
-// - program.option: the options that this command can take, if any
-// - program.action: the action that this command performs, which will be a function
-
-// all commands for cli:
-// - ✅actions list
-// - ✅actions list --today
-// - ✅actions list --yesterday 
-// - ✅actions add "get milk" "buy bike"
-// - ✅actions remove --items 1 2
-// - ✅actions remove --last
-// - actions done --items 1 2
-// - actions filter date=1/1/2020
-// - actions starts-with="proj:bob"
+const { program } = require("commander");
+const addActionItem = require("./utils/addActionItem");
+const filterActionItems = require("./utils/filterActionItems");
+const listActionItems = require("./utils/listActionItems");
+const markDoneActionItems = require("./utils/markDoneActionItems");
+const markUndoneActionItems = require("./utils/markUndoneActionItems");
+const removeActionItems = require("./utils/removeActionItems");
 
 program
-  .command('list')
-  .description('Lists all of your actions items, i.e., `actions list`')
-  .option('-t, --today', 'All the action items for today')
-  .option('-y, --yesterday', 'All the action items from yesterday')
-  .action(listActionItems)
+  .command("list")
+  .description(
+    "Lists your actions items\ni.e.\n- `actions list` (lists all of your actions items)\n- `actions list --today` (lists all of your actions items for today)\n- `actions list --yesterday` (lists all of your actions items from yesterday)"
+  )
+  .option("-t, --today", "All the action items for today")
+  .option("-y, --yesterday", "All the action items from yesterday")
+  .action(listActionItems);
 
 program
-  .command('add <actions...>')
-  .description('Add new action item(s), i.e., `actions add "buy milk" "do homework"`')
-  .action(addActionItem)
+  .command("add <actions...>")
+  .description(
+    'Add new action item(s), i.e., `actions add "buy milk" "do homework"`'
+  )
+  .action(addActionItem);
 
 program
-  .command('remove')
-  .description('Remove action item(s)\ni.e.\n- `actions remove --last` (removes the most recent action item added to list)\n- `actions remove --items 2 3` (removes the provided action ids, in this case, ids 2 and 3)')
-  .option('-l, --last', 'Removes the most recent action item added to list')
-  .option('-i, --items <items...>', 'Removes the provided action ids, i.e., (1)')
-  .action(removeActionItems)
+  .command("remove")
+  .description(
+    "Remove action item(s)\ni.e.\n- `actions remove --last` (removes the most recent action item added to list)\n- `actions remove --items 2 3` (removes the provided action ids, in this case, ids 2 and 3)"
+  )
+  .option("-l, --last", "Removes the most recent action item added to list")
+  .option(
+    "-i, --items <items...>",
+    "Removes the provided action ids, i.e., (1)"
+  )
+  .action(removeActionItems);
 
 program
-  .command('done')
-  .description('Marks action item(s) as complete\ni.e.\n- `actions done --all` (marks all the action items as complete)\n- `actions done --last` (Marks the most recent action item as complete)\n- `actions done --items 2 3` (marks the provided action ids as done, in this case, ids 2 and 3)')
-  .option('-a, --all', 'Marks all the action items as complete')
-  .option('-l, --last', 'Marks the most recent action item as complete')
-  .option('-i, --items <items...>', 'Marks the provided action ids as complete, i.e., (1)')
-  .action(markDoneActionItems)
+  .command("done")
+  .description(
+    "Marks action item(s) as complete\ni.e.\n- `actions done --all` (marks all the action items as complete)\n- `actions done --last` (Marks the most recent action item as complete)\n- `actions done --items 2 3` (marks the provided action ids as done, in this case, ids 2 and 3)"
+  )
+  .option("-a, --all", "Marks all the action items as complete")
+  .option("-l, --last", "Marks the most recent action item as complete")
+  .option(
+    "-i, --items <items...>",
+    "Marks the provided action ids as complete, i.e., (1)"
+  )
+  .action(markDoneActionItems);
 
 program
-  .command('undone')
-  .description('Marks action item(s) as complete\ni.e.\n- `actions undone --all` (marks all the action items as not complete)\n- `actions undone --last` (Marks the most recent action item as not complete)\n- `actions undone --items 2 3` (marks the provided action ids as not done, in this case, ids 2 and 3)')
-  .option('-a, --all', 'Marks all the action items as not complete')
-  .option('-l, --last', 'Marks the most recent action item as not complete')
-  .option('-i, --items <items...>', 'Marks the provided action ids as not complete, i.e., (1)')
-  .action(markUndoneActionItems)
+  .command("undone")
+  .description(
+    "Marks action item(s) as complete\ni.e.\n- `actions undone --all` (marks all the action items as not complete)\n- `actions undone --last` (Marks the most recent action item as not complete)\n- `actions undone --items 2 3` (marks the provided action ids as not done, in this case, ids 2 and 3)"
+  )
+  .option("-a, --all", "Marks all the action items as not complete")
+  .option("-l, --last", "Marks the most recent action item as not complete")
+  .option(
+    "-i, --items <items...>",
+    "Marks the provided action ids as not complete, i.e., (1)"
+  )
+  .action(markUndoneActionItems);
+
+program
+  .command("filter")
+  .description(
+    'Filters action item(s) based on given flags\ni.e.\n- `actions filter --date 6/10/2022` (filters action items created on 6/10/2022, format [MONTH]/[DATE]/[YEAR])\n- `actions filter --includes "[PROJ-43545]"` (filters action items that includes the text ("[PROJ-43545]") in it (Note: either text or regex are allowed as a parameter)'
+  )
+  .option(
+    "-d, --date <date>",
+    "Filters action items created on the provided date"
+  )
+  .option(
+    "-sw, --includes <content>",
+    "Filters action items that includes provided text"
+  )
+  .action(filterActionItems);
 
 program.parse();
--- src/utils/listActionItems.js
+++ src/utils/listActionItems.js
@@ -1,67 +1,61 @@
-const chalk = require('chalk');
-const config = require('../config');
-const { ACTIONS_LIST } = require('../constants');
-const getYesterdayDate = require('./getYesterdayDate');
+const chalk = require("chalk");
+const config = require("../config");
+const { ACTIONS_LIST } = require("../constants");
+const getYesterdayDate = require("./getYesterdayDate");
 
 function listActionItems(options = {}) {
   const { today = false, yesterday = false } = options;
   const allActions = config.get(ACTIONS_LIST);
   if (!allActions || !allActions.length) {
-    console.log(
-      chalk.green('Nice! You don\'t have any action items 👌')
-    )
+    console.log(chalk.green("Nice! You don't have any action items 👌"));
   } else if (today) {
-    const actionsForToday = allActions
-      .filter((action) => {
-        return (new Date(action.createdAt)).toDateString() === (new Date()).toDateString()
-      });
+    const actionsForToday = allActions.filter((action) => {
+      return (
+        new Date(action.createdAt).toDateString() === new Date().toDateString()
+      );
+    });
     console.log(
       chalk.gray.bold(`List of all your actions for`),
-      chalk.green.underline('TODAY'),
-      chalk.gray.bold(`(Total: ${actionsForToday.length}): ✅ COMPLETE | ❌ NOT COMPLETE \n`)
+      chalk.green.underline("TODAY"),
+      chalk.gray.bold(
+        `(Total: ${actionsForToday.length}): ✅ COMPLETE | ❌ NOT COMPLETE \n`
+      )
     );
-    actionsForToday.forEach((action = {}, index) => (
+    actionsForToday.forEach((action = {}, index) =>
       action.complete
-        ? console.log(
-          chalk.green.bold(`✅ (${index + 1}) ${action.text}`)
-        )
-        : console.log(
-          chalk.gray.bold(`❌ (${index + 1}) ${action.text}`)
-        )
-    ))
+        ? console.log(chalk.green.bold(`✅ (${index + 1}) ${action.text}`))
+        : console.log(chalk.gray.bold(`❌ (${index + 1}) ${action.text}`))
+    );
   } else if (yesterday) {
-    const actionsFromYesterday = allActions
-      .filter((action) => {
-        return (new Date(action.createdAt)).toDateString() === getYesterdayDate().toDateString()
-      });
+    const actionsFromYesterday = allActions.filter((action) => {
+      return (
+        new Date(action.createdAt).toDateString() ===
+        getYesterdayDate().toDateString()
+      );
+    });
     console.log(
       chalk.gray.bold(`List of all your actions from`),
-      chalk.green.underline('YESTERDAY'),
-      chalk.gray.bold(`(Total: ${actionsFromYesterday.length}): ✅ COMPLETE | ❌ NOT COMPLETE \n`)
+      chalk.green.underline("YESTERDAY"),
+      chalk.gray.bold(
+        `(Total: ${actionsFromYesterday.length}): ✅ COMPLETE | ❌ NOT COMPLETE \n`
+      )
     );
-    actionsFromYesterday
-      .forEach((action = {}, index) => (
+    actionsFromYesterday.forEach((action = {}, index) =>
       action.complete
-        ? console.log(
-          chalk.green.bold(`✅ (${index + 1}) ${action.text}`)
-        )
-        : console.log(
-          chalk.gray.bold(`❌ (${index + 1}) ${action.text}`)
-        )
-    ))
+        ? console.log(chalk.green.bold(`✅ (${index + 1}) ${action.text}`))
+        : console.log(chalk.gray.bold(`❌ (${index + 1}) ${action.text}`))
+    );
   } else {
     console.log(
-      chalk.gray.bold(`List of all your actions (Total: ${allActions.length}): ✅ COMPLETE | ❌ NOT COMPLETE \n`)
-    )
-    allActions.forEach((action = {}, index) => (
+      chalk.gray.bold(
+        `List of all your actions (Total: ${allActions.length}): ✅ COMPLETE | ❌ NOT COMPLETE \n`
+      )
+    );
+    allActions.forEach((action = {}, index) =>
       action.complete
-        ? console.log(
-          chalk.green(`✅ (${index + 1}) ${action.text}`)
-        )
-        : console.log(
-          chalk.gray(`❌ (${index + 1}) ${action.text}`)
-        )
-    ))
+        ? console.log(chalk.green(`✅ (${index + 1}) ${action.text}`))
+        : console.log(chalk.gray(`❌ (${index + 1}) ${action.text}`))
+    );
   }
 }
 
